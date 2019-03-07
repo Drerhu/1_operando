26c956a869d0d167ca207e7350ce2551113abbbd
Andres Masset <masset.ruiz151@gmail.com>
2019-03-07 10:15:56 +0100

    CRETARIO UPDATE

--- src/Suma.java
+++ src/Suma.java
@@ -3,7 +3,7 @@ public class Suma {
 public static void main (String[]args) {
 int num = 1;
 int num1 = 2;
-int resultado=0;
+int resultado=num+num1;
 
 System.out.println(num+" + "+num1+" = "+resultado);
 }
